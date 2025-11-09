b91ef2698214c2caa1c74f4823142f9448ee7a5c
chantaeh <chantae.ho@ucalgary.ca>
2025-11-08 22:11:48 -0700

    Add forgot password

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -9,7 +9,15 @@ interface LoginProps {
 function Login({ setIsLoggedIn }: LoginProps) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [showForgot, setShowForgot] = useState(false);
+  const [email, setEmail] = useState("");
   const navigate = useNavigate();
+
+  const handleOk = () => {
+    console.log("Email entered:", email);
+    setShowForgot(false); // close modal
+    setEmail(""); // reset input
+  };
 
   const handleLogin = (e: React.FormEvent) => {
     e.preventDefault();
@@ -54,6 +62,34 @@ function Login({ setIsLoggedIn }: LoginProps) {
           Login
         </button>
       </form>
+
+      <p onClick={() => setShowForgot(true)} id="forgot-password">Forgot Password?</p>
+
+      {showForgot && (
+        <div style={{
+          position: "fixed", top: 0, left: 0, width: "100%", height: "100%",
+          backgroundColor: "rgba(0,0,0,0.5)", display: "flex", justifyContent: "center", alignItems: "center"
+        }}>
+          <div style={{ background: "white", padding: "20px", borderRadius: "8px" }}>
+            <h3>Forgot Password</h3>
+            <input
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              style={{
+                width: '100%',
+                padding: '0.5rem',
+                border: '1px solid #abaaaaff',
+                borderRadius: '4px',
+                boxSizing: 'border-box'}}
+            />
+            <button onClick={handleOk} className='submit-button' style={{
+              margin: '1rem'
+            }}>OK</button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
